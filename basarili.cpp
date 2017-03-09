d740614550f263b6e730e8ae719d3af6b99f5ca6
caglaozturk <ozturkcagla041@gmail.com>
2017-03-09 20:27:13 +0300

    Update basarili.cpp

--- basarili.cpp
+++ basarili.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-		int ogrenci[5],sayac=0;
-		for(int i=0;i<5;i++){
+		int ogrenci[10],sayac=0;
+		for(int i=0;i<10;i++){
 			printf("%d.kisinin notunu gir  :",i+1);
 			scanf("%d",&ogrenci[i]);
 			if(ogrenci[i]>=60)
