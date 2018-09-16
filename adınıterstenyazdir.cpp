b14a3408f38881f24cdc2216eff2880bafd24dcc
Çağla Öztürk <ozturkcagla041@gmail.com>
2018-09-17 00:55:57 +0300

    Adını Tersten Yazdır
    Bir String i Tersten Yazdırma

---
+++ adınıterstenyazdir.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <conio.h> 
+#include <string.h> 
+  int main() { 
+       int i,j, temp,k; 
+       char str[100]="caglaozturk";
+       for(i=0; str[i]!='\0'; i++){     } 
+       for(j=i; j>=0; j--) { 
+            printf("%c",str[j]); 
+       } 
+   
+       getch(); 
+       return 0; 
+  } 
